da5b35fa4307aa77c8ce3d79a48b71644155464d
debora-cristinaf <programacaodcrisf@gmail.com>
2024-05-29 12:46:39 -0300

    tests: fix cases

--- src/test/java/com/example/pi_test/CalculatorTest.java
+++ src/test/java/com/example/pi_test/CalculatorTest.java
@@ -10,27 +10,17 @@ class CalculatorTest {
 
 //    @Test
 //    void isPrime() {
-//        calculator.isPrime(1);
-//        calculator.isPrime(2);
-//        assertTrue(true);
+//        assertTrue(calculator.isPrime(2));
 //    }
 
     @Test
-    public void testIsPrime() {
-        Calculator calc = new Calculator();
+    public void shouldVerifyIsPrime() {
 
-        assertFalse(calc.isPrime(-1), "-1 should not be prime");
-        assertFalse(calc.isPrime(0), "0 should not be prime");
-        assertFalse(calc.isPrime(1), "1 should not be prime");
-        assertTrue(calc.isPrime(2), "2 should be prime");
-        assertTrue(calc.isPrime(3), "3 should be prime");
-        assertFalse(calc.isPrime(4), "4 should not be prime");
-        assertTrue(calc.isPrime(5), "5 should be prime");
-        assertFalse(calc.isPrime(6), "6 should not be prime");
-        assertTrue(calc.isPrime(7), "7 should be prime");
-        assertFalse(calc.isPrime(8), "8 should not be prime");
-        assertFalse(calc.isPrime(9), "9 should not be prime");
-        assertFalse(calc.isPrime(10), "10 should not be prime");
-        assertTrue(calc.isPrime(11), "11 should be prime");
+        assertFalse(calculator.isPrime(-1), "-1 should not be prime");
+        assertFalse(calculator.isPrime(0), "0 should not be prime");
+        assertFalse(calculator.isPrime(1), "1 should not be prime");
+        assertTrue(calculator.isPrime(2), "2 should be prime");
+        assertFalse(calculator.isPrime(4), "4 should not be prime");
+        assertTrue(calculator.isPrime(7), "7 should be prime");
     }
 }
